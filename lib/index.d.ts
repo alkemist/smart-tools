260dde589f35fd5c779d223b262bef523ce19160
Jérémy ACHAIN <achain.jeremy@gmail.com>
2023-11-30 21:41:06 +0100

    Improves

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './helpers/index.js';
-export * from './managers/index.js';
-export * from './models/index.js';
-export * from './tools/index.js';
-//# sourceMappingURL=index.d.ts.map
--- lib/managers/smart-loader.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=smart-loader.d.ts.map
